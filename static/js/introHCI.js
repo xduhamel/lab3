47c34f2c0b44f76430d0e92ad63430d4081bca93
Matt Duhamel <mduhamel@stanford.edu>
2014-01-22 16:25:20 -0800

    javascript lab

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -15,4 +15,35 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+    $("a.thumbnail").click(projectClick);
+    $("#submitBtn").click(updateProject);
+}
+
+function updateProject(e){
+    var projectID = $('#project').val();
+    $(projectID).animate({
+        width: $('width').val()
+    });
+
+    var newText = $('#description').val();
+    $(projectID + " .project-description").text(newText);
+}
+
+function projectClick(e) {
+    e.preventDefault();
+    var projectTitle = $(this).find("p").text();
+    var jumbotronHeader = $(".jumbotron h1");
+    
+    $("#testjs").text("Please wait...");
+    $(".jumbotron p").toggleClass("active");
+    jumbotronHeader.text(projectTitle); 
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    
+    if (description.length == 0) {
+        $(containingProject).append("<div class='project-description'><p>Project Description</p></div>");
+    } else {
+        description.html("<p>Stop clicking the button!  You just did it at " + (new Date()) + "</p>");
+    }
 }
